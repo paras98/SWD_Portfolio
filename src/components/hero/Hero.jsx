b608a9b18f809027c85c2c9095b90c7f4a50dfb7
pskali1998 <pompolompo2@gmail.com>
2024-04-09 16:16:36 -0400

    updated header

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,7 +8,7 @@ import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
 import { FiDownload } from "react-icons/fi";
 import { FaPlay } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
-import resumePDF from "../../assets/PARAS_SAXENA_FTW.pdf";
+import resumePDF from "../../assets/FullStackResume.pdf";
 
 
 const Hero = () => {
